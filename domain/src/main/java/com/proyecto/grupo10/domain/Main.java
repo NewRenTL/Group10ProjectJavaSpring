78c6bdd0be56284f005cf434a6e229baa1557e70
Kazuto64 <rcarlos66@yahoo.com>
2024-03-27 20:31:36 -0500

    commit

--- domain/src/main/java/com/proyecto/grupo10/domain/Main.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.grupo10.domain;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- infrastructure/src/main/java/com/proyecto/grupo10/infrastructure/Main.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.grupo10.infrastructure;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
